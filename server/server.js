a8b964ac50b48c0df00dd66e7551dfde30b05be6
Jennifer Peavler <jenn.peavler@gmail.com>
2017-06-26 15:17:49 -0600

    initial commit, set up server, static files, and served them

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use('/assets', express.static(path.join(__dirname, '../client/assets/')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
