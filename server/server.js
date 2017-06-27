8c05f9f6b7b35ae803a42c8840629462bf303dc2
Devon Bull <dbull@live.com>
2017-06-26 18:20:16 -0600

    body parser was required, now creating new folders

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const router = require('./router')
+const bodyParser = require('body-parser')
+
+
+
+app.use(bodyParser.json());
 
 app.use('/assets', express.static(path.join(__dirname, '../client/assets/')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
